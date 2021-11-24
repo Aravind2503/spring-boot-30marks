1035394c322468355e4df78445ae302168f1af5f
Kraken676 <arunkennedy78@gmail.com>
2021-11-24 22:45:09 +0530

    second

--- hotels/src/main/java/com/example/hotels/controller/HotelController.java
+++ hotels/src/main/java/com/example/hotels/controller/HotelController.java
@@ -1,5 +1,63 @@
+// package com.example.hotelCrud.controller;
+
+// import com.example.hotelCrud.model.hotel;
+// import com.example.hotelCrud.service.hotelService;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.util.List;
+
 package com.example.hotels.controller;
 
+import com.example.hotels.model.Hotel;
+import com.example.hotels.service.HotelService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@RestController
 public class HotelController {
-    
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping("/reservationForm")
+    public String reservationForm(Model model) {
+      model.addAttribute("reservationForm", new Hotel());
+      return "reservationForm";
+    }
+
+    @GetMapping("/getAllhotels")
+    public List<Hotel> getAllhotels() {
+        return hotelService.getAllhotels();
+    }
+
+    @GetMapping("/get/{hotelID}")
+    public Hotel gethotel(@PathVariable String hotelID) {
+        return hotelService.gethotel(hotelID);
+    }
+
+    @PostMapping("/createhotel")
+    public Hotel createhotel(@RequestBody Hotel hotel) {
+        return hotelService.create(hotel);
+    }
+
+    @DeleteMapping("/deletehotel/{hotelId}")
+    public String deletehotel(@PathVariable String hotelId) {
+        hotelService.delete(hotelId);
+        return "hotel Deleted";
+    }
+
+    @PutMapping("/updatehotel/{hotelId}")
+    public Hotel updatehotel(@RequestBody Hotel hotel, @PathVariable String hotelId) {
+        return hotelService.update(hotel, hotelId);
+    }
+
+    @DeleteMapping("/deleteAll")
+    public String deletehotels() {
+        hotelService.deleteAll();
+        return "All hotels data deleted";
+    }
 }
---
+++ hotels/src/main/java/com/example/hotels/service/HotelService.java
@@ -0,0 +1,46 @@
+package com.example.hotels.service;
+
+import com.example.hotels.model.Hotel;
+import com.example.hotels.repository.HotelRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HotelService {
+
+    private final HotelRepository hotelRepository;
+
+    public HotelService(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
+    public List<Hotel> getAllhotels() {
+        return hotelRepository.findAll();
+    }
+
+    public Hotel gethotel(String hotelID) {
+        return hotelRepository.findById(hotelID).orElse(null);
+    }
+
+
+    public Hotel create(Hotel hotel) {
+        return hotelRepository.save(hotel);
+    }
+
+    public void delete(String hotelId) {
+        hotelRepository.deleteById(hotelId);
+    }
+
+    public Hotel update(Hotel hotel, String hotelId) {
+        Hotel hotel1 = hotelRepository.findById(hotelId).get();
+        hotel1.setAuthor(hotel.getAuthor());
+        hotel1.setPublisher(hotel.getPublisher());
+        hotelRepository.save(hotel1);
+        return hotel1;
+    }
+
+    public void deleteAll() {
+        hotelRepository.deleteAll();
+    }
+}
