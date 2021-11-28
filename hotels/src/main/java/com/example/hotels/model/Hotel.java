406a13287de9219d3801c5ef7a3e315b2d5f5148
Kraken676 <arunkennedy78@gmail.com>
2021-11-28 22:05:24 +0530

    second latest code

--- hotels/src/main/java/com/example/hotels/model/Hotel.java
+++ hotels/src/main/java/com/example/hotels/model/Hotel.java
@@ -18,15 +18,20 @@ public class Hotel {
     private String address;
     @Column
     private int numRooms;
+    @Column 
+    private int price;
 
-    public Hotel() {
-
-    }
-
-    public Hotel(String string, String string2, String string3) {
-        hotelId = string;
-        hname = string2;
-        oname = string3;
+	public Hotel()
+	{
+		
+	}
+    public Hotel(String string, String string2, String string3,String s4,int nRooms,int pr) {
+        hotelId=string;
+		hname=string2;
+		oname=string3;
+        address=s4;
+        numRooms=nRooms;
+        price=pr;
     }
 
     public String getId() {
@@ -69,4 +74,89 @@ public class Hotel {
         this.numRooms = numRooms;
     }
 
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int pr) {
+        this.price = pr;
+    }
+
+
+
 }
+
+
+
+// package com.example.hotels.model;
+
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
+
+// @Entity
+// @Table
+// public class Hotel {
+//     @Id
+//     private String hotelId;
+//     @Column
+//     private String hname;
+//     @Column
+//     private String oname;
+//     @Column
+//     private String address;
+//     @Column
+//     private int numRooms;
+
+//     public Hotel() {
+
+//     }
+
+//     public Hotel(String string, String string2, String string3) {
+//         hotelId = string;
+//         hname = string2;
+//         oname = string3;
+//     }
+
+//     public String getId() {
+//         return hotelId;
+//     }
+
+//     public void setId(String id) {
+//         this.hotelId = id;
+//     }
+
+//     public String getHname() {
+//         return hname;
+//     }
+
+//     public void setHname(String hname) {
+//         this.hname = hname;
+//     }
+
+//     public String getOname() {
+//         return oname;
+//     }
+
+//     public void setOname(String oname) {
+//         this.oname = oname;
+//     }
+
+//     public String getAddress() {
+//         return address;
+//     }
+
+//     public void setAddress(String address) {
+//         this.address = address;
+//     }
+
+//     public int getNumRooms() {
+//         return numRooms;
+//     }
+
+//     public void setNumRooms(int numRooms) {
+//         this.numRooms = numRooms;
+//     }
+
+// }
